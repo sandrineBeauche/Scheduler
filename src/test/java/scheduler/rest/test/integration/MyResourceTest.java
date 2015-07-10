999ba5963c87aa091ad5239ce24a986fea4ee44d
Sandrine Beauche <sandrine.beauche@gmail.com>
2015-07-10 17:57:03 +0200

    add integration test exemple

---
+++ src/test/java/scheduler/rest/test/integration/MyResourceTest.java
@@ -0,0 +1,26 @@
+package scheduler.rest.test.integration;
+
+import static org.junit.Assert.*;
+
+import javax.ws.rs.core.Application;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import scheduler.rest.MyResource;
+
+public class MyResourceTest extends JerseyTest {
+
+	@Override
+    protected Application configure() {
+        return new ResourceConfig(MyResource.class);
+    }
+	
+	@Test
+    public void test1() {
+        final String hello = target("myresource").request().get(String.class);
+        assertEquals("Got it!", hello);
+    }
+	
+}
